caf8ea73e72128cceea2cf42f42d758aa8073498
Juan Carlos Medina Sánchez <150171244+JuaMedCa@users.noreply.github.com>
2024-06-21 22:58:47 -0600

    Actualizar Inicio.jsx

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -7,7 +7,7 @@ const Inicio = () => (
             <h1 className='titulo'>FitChair</h1>
             <p className='texto'>En FitChair somos un equipo de profesionales dedicados 
                 a la innovación y el bienestar. Nuestra misión es mejorar la experiencia de la 
-                comunidad gamer mediante la integración de tecnología avanzada y soluciones de salud.</p>
+                comunidad gamer mediante la integración de una silla que te permitira saber el estado de tu peso cuántas horas pasas sentado en el escritorio mediante sensores que tendrá incorporado la silla y esto es mediante tecnología avanzada y soluciones de salud.</p>
         
         </div>
     </div>
