cfda9956c7b0514b0539d61a000b62ee2ec848e6
Juan Carlos Medina Sánchez <150171244+JuaMedCa@users.noreply.github.com>
2024-07-04 12:50:04 -0600

    cambio6

--- src/componentes/Inicio.jsx
+++ src/componentes/Inicio.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import "../inicio.css"
 
 const Inicio = () => (
-    <div>
-        <div>
+    <div className='inicio'>
             <h1 className='titulo'>FitChair</h1>
+            <div className='fondo'>
             <p className='texto'>En FitChair somos un equipo de profesionales dedicados 
                 a la innovación y el bienestar. Nuestra misión es mejorar la experiencia de la 
                 comunidad gamer mediante la integración de una silla que te permitira saber el estado de tu peso cuántas horas pasas sentado en el escritorio mediante sensores que tendrá incorporado la silla y esto es mediante tecnología avanzada y soluciones de salud.</p>
-        
-        </div>
+    
+            </div>
+            
     </div>
 );
 
